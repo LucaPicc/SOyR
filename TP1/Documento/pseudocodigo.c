fc45f0eecdd5f0e34638afe376d265ebb102ecd7
LucaPicc <luca.piccinini0009@gmail.com>
2021-04-15 20:36:28 -0300

    Corregimos informe y pseudocodigo

--- TP1/Documento/pseudocodigo.c
+++ TP1/Documento/pseudocodigo.c
@@ -1,20 +1,26 @@
+Asignación de la dirección del directorio creado a la variable dir
+Asignación de la dirección del archivo creado a la variable file
 
-if(verificamos que la cantidad de arguntas sean 2){
-    crea directorio con nombre ingresado como primer argumento en home;
-    
-    copia los archivos con extension .conf en el directorio creado;
-    
-    se quitan todos los permisos a grupo y al resto del mundo a todos 
-    los archivos cuya tercera letra de su nombre sea una "g";
-    
-    se copian los primeros 5 archivos en orden creciente por tamaño en
-    el archivo creado, con nombre del segundo argumento;
-    
-    se copian los últimos 5 archivos ordenados alfabeticamente en orden 
-    decreciente al final del archivo;
-}
-else{
-    se imprime mensaje de error
-}
-FIN
+if [número de argumentos pasados es distinto de 2];
+then
+	Mensaje explicativo de ERROR;
+elif [se pasa un punto “.” o una barra “/” en alguno de los argumentos];
+then
+	Mensaje explicativo de ERROR;
+elif [ya existe un directorio con el nombre pasado];
+then
+	Mensaje explicativo de ERROR;
+elif [ya existe un archivo con el nombre pasado en HOME];
+then
+	Mensaje explicativo de ERROR;
+else
 
+	Se crea el directorio en dir
+	Se buscan y se copian los archivos que terminan en “.conf” y donde el resto del mundo tienen permisos de lectura al directorio
+
+	Se quitan todos los permisos para grupos y resto del mundo
+
+	Se escriben en file, del contenido del directorio listado en orden creciente de tamaño, los primeros 5 archivos 
+	Se escriben a continuación, del contenido del directorio listado alfabética y decrecientemente, los últimos 5 archivos.
+
+fi
